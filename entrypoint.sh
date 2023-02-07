28a0cee60a82b97a79af81da59a3e342a4841834
Chetan Chaudhari <31230509+Cschau0701vip@users.noreply.github.com>
2023-02-07 22:13:54 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,51 @@
 set -e
 
 echo ${INPUT_TITLE}
+
+header='{
+          "type": "header",
+          "text": {
+          "type": "plain_text",
+          "text": "${{ inputs.title }}"
+          }
+        },'
+        
+divider='{
+  "type": "divider"
+},'
+
+# Slack markdown doesn't accept empty `text`
+if [ ! -z '${{ inputs.body }}' ]
+then
+  body='{
+    "type": "section",
+    "text": {
+    "type": "mrkdwn",
+    "text": "${{ inputs.body }}"
+    }
+  },'
+fi
+
+context='{
+  "type": "context",
+  "elements": [
+  {
+    "type": "plain_text",
+    "text": "${{ inputs.context }}"
+  }
+  ]
+}'
+
+blocks='"blocks": [
+  '$header'
+  '$divider'
+  '$body'
+  '$context'
+]'
+
+data='{'$blocks'}'
+
+output=$(curl ${{ inputs.webhook_url }} \
+  --request POST \
+  --header 'Content-type: application/json' \
+  --data "$data" | jq)
